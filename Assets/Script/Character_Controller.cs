879005cdc0898adcc93a14a784022e0aa8663717
slarelerouge <slarelerouge@users.noreply.github.com>
2017-01-28 16:08:37 +0100

    Revert "Revert "Project foundation""
    This reverts commit ed7155af484552f7a895f730ab3fa3e25e74babf.

---
+++ Assets/Script/Character_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour 
+{
+//	private Rigidbody rb;
+
+	void Start () 
+	{
+//		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+//		float moveVertical = Input.GetAxis("Vertical");
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+
+//		rb.AddForce (movement);
+
+		GameObject BezierPath = GameObject.Find("BezierPath");
+		//BezierPath.GetComponent<BezierPath>().BezierPath.GetPositionByDistance (Time.time/100);
+		//Vector3 position = BezierScript.BezierPath.GetPositionByDistance (Time.time/100);
+
+		transform.position = BezierPath.GetComponent<BezierPath>().GetPositionByDistance(Time.time*10);
+	}
+}
+ 
