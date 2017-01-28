7e777fca9be493864d59a77f658043ff13f7fbda
slarelerouge <arnouldpaulga@gmail.com>
2017-01-28 15:32:28 +0100

    Project foundation
    just the very foundation

---
+++ Assets/Script/SplineScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineScript : MonoBehaviour {
+	public Vector3 splinePosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		splinePosition = GameObject.BezierPath.GetPositionByDistance (Time.time);
+//		PlayerScript splineScript = thePlayer.GetComponent<PlayerScript>();
+	}
+}
